fa7bdfb92626d20fb8c68d45cd48d3451eb153b4
bibhasroy93 <butunroy@gmail.com>
2021-09-20 14:43:56 +0530

    20092021

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
+const slideImages = [
+  {
+    url: "https://i.ibb.co/prtKnkv/slider1.jpg",
+    //caption: 'Slide my1'
+  },
+  {
+    url: "https://i.ibb.co/2hdkjpw/slider2.jpg",
+    //caption: 'Slide 2'
+  },
+  {
+    url: "https://i.ibb.co/tKwCWrb/slider3.jpg",
+    //caption: 'Slide 3'
+  },
+];
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return <div>
+    <div className="slide-container">
+        <Slide>
+          {slideImages.map((slideImage, index) => (
+            <div className="each-slide" key={index}>
+              <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))}
+        </Slide>
+      </div>
+  </div>;
 };
 
 export default HomePage;
